c8b06b5fe70a123709b2c90891eb8ceeb69263ec
SpootyMcSpoot <TheItCrowdSurfer@gmail.com>
2026-04-29 22:35:31 -0700

    feat(ui): aria-hidden on Select + DropdownMenu decorative icons (pass119) (#216)
    - Select trigger chevron, item check, scroll up/down chevrons
    - DropdownMenu item check, radio circle, sub-menu chevron
    - Marking decorative so screen readers don't announce raw glyphs
      alongside the menu / item labels

--- ui/src/components/ui/select.tsx
+++ ui/src/components/ui/select.tsx
@@ -42,7 +42,7 @@ function SelectTrigger({
     >
       {children}
       <SelectPrimitive.Icon asChild>
-        <ChevronDownIcon className="size-4 opacity-50" />
+        <ChevronDownIcon className="size-4 opacity-50" aria-hidden="true" />
       </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
   )
@@ -117,7 +117,7 @@ function SelectItem({
         className="absolute right-2 flex size-3.5 items-center justify-center"
       >
         <SelectPrimitive.ItemIndicator>
-          <CheckIcon className="size-4" />
+          <CheckIcon className="size-4" aria-hidden="true" />
         </SelectPrimitive.ItemIndicator>
       </span>
       <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
@@ -151,7 +151,7 @@ function SelectScrollUpButton({
       )}
       {...props}
     >
-      <ChevronUpIcon className="size-4" />
+      <ChevronUpIcon className="size-4" aria-hidden="true" />
     </SelectPrimitive.ScrollUpButton>
   )
 }
@@ -169,7 +169,7 @@ function SelectScrollDownButton({
       )}
       {...props}
     >
-      <ChevronDownIcon className="size-4" />
+      <ChevronDownIcon className="size-4" aria-hidden="true" />
     </SelectPrimitive.ScrollDownButton>
   )
 }
